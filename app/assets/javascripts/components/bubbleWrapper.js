2db2abda3d075bcdfdca8131b3a66e95d04aa3a2
Valoch <val.selve@gmail.com>
2018-10-20 22:39:42 +0200

    make home and bubbbles

---
+++ app/assets/javascripts/components/bubbleWrapper.js
@@ -0,0 +1,57 @@
+$(".bubble_1").click(function () {
+  $(".bubble_1_wrapper").removeClass("hidden");
+});
+
+$(".bubble_1_wrapper").click(function () {
+  $(".bubble_1_wrapper").addClass("hidden");
+});
+
+// SECOND
+
+$(".bubble_2").click(function () {
+  $(".bubble_2_wrapper").removeClass("hidden");
+});
+
+$(".bubble_2_wrapper").click(function () {
+  $(".bubble_2_wrapper").addClass("hidden");
+});
+
+// THIRD
+
+$(".bubble_3").click(function () {
+  $(".bubble_3_wrapper").removeClass("hidden");
+});
+
+$(".bubble_3_wrapper").click(function () {
+  $(".bubble_3_wrapper").addClass("hidden");
+});
+
+// FOURTH
+
+$(".bubble_4").click(function () {
+  $(".bubble_4_wrapper").removeClass("hidden");
+});
+
+$(".bubble_4_wrapper").click(function () {
+  $(".bubble_4_wrapper").addClass("hidden");
+});
+
+// FIFTH
+
+$(".bubble_5").click(function () {
+  $(".bubble_5_wrapper").removeClass("hidden");
+});
+
+$(".bubble_5_wrapper").click(function () {
+  $(".bubble_5_wrapper").addClass("hidden");
+});
+
+// SIXTH
+
+$(".bubble_6").click(function () {
+  $(".bubble_6_wrapper").removeClass("hidden");
+});
+
+$(".bubble_6_wrapper").click(function () {
+  $(".bubble_6_wrapper").addClass("hidden");
+});
