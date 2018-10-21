9bc52a7c45de6ee3eede083944ec09600b0e1bc2
Valoch <val.selve@gmail.com>
2018-10-21 22:30:21 +0200

    better shadows

---
+++ app/assets/javascripts/components/animation.js
@@ -0,0 +1,107 @@
+// 1
+setInterval(function(){
+  $(".bubble_1").animate({
+      top: "41%"
+  }, 945 );
+  $(".bubble_shadow_1").animate({
+      width: "135px"
+  }, 945 );
+}, 1900);
+setInterval(function(){
+  $(".bubble_1").animate({
+      top: "39%"
+  }, 940 );
+  $(".bubble_shadow_1").animate({
+      width: "125px"
+  }, 940 );
+}, 1900);
+
+// 2
+setInterval(function(){
+  $(".bubble_2").animate({
+      top: "37%"
+  }, 941 );
+  $(".bubble_shadow_2").animate({
+      width: "95px"
+  }, 941 );
+}, 1900);
+setInterval(function(){
+  $(".bubble_2").animate({
+      top: "39%"
+  }, 944 );
+  $(".bubble_shadow_2").animate({
+      width: "105px"
+  }, 944 );
+}, 1900);
+
+// 3
+setInterval(function(){
+  $(".bubble_3").animate({
+      bottom: "29%"
+  }, 941 );
+  $(".bubble_shadow_3").animate({
+      width: "31px"
+  }, 941 );
+}, 1900);
+setInterval(function(){
+  $(".bubble_3").animate({
+      bottom: "31%"
+  }, 946 );
+  $(".bubble_shadow_3").animate({
+      width: "21px"
+  }, 946 );
+}, 1900);
+
+// 4
+setInterval(function(){
+  $(".bubble_4").animate({
+      bottom: "18%"
+  }, 940 );
+  $(".bubble_shadow_4").animate({
+      width: "53px"
+  }, 940 );
+}, 1900);
+setInterval(function(){
+  $(".bubble_4").animate({
+      bottom: "20%"
+  }, 944 );
+  $(".bubble_shadow_4").animate({
+      width: "43px"
+  }, 944 );
+}, 1900);
+
+// 5
+setInterval(function(){
+  $(".bubble_5").animate({
+      bottom: "11%"
+  }, 946 );
+  $(".bubble_shadow_5").animate({
+      width: "96px"
+  }, 946 );
+}, 1900);
+setInterval(function(){
+  $(".bubble_5").animate({
+      bottom: "9%"
+  }, 940 );
+  $(".bubble_shadow_5").animate({
+      width: "116px"
+  }, 940 );
+}, 1900);
+
+// 6
+setInterval(function(){
+  $(".bubble_6").animate({
+      top: "37%"
+  }, 942 );
+  $(".bubble_shadow_6").animate({
+      width: "65px"
+  }, 942 );
+}, 1900);
+setInterval(function(){
+  $(".bubble_6").animate({
+      top: "39%"
+  }, 945 );
+  $(".bubble_shadow_6").animate({
+      width: "75px"
+  }, 945 );
+}, 1900);
